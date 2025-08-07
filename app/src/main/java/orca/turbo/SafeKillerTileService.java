d21a6592e02db181668a40db429ebc7b9c1d2902
maxMaxim59 <62408960+maxMaxim59@users.noreply.github.com>
2025-08-07 12:53:03 -0300

    Update SafeKillerTileService.java
    feat: Expanded SafeKiller's game exclusion list
    
    This commit expands the SafeKiller service's exclusion list to include a large number of new game titles. This ensures that these games are not force-stopped when "Turbo" mode is activated, allowing for an uninterrupted gaming experience.
    
    List of games added:
    
    * **Mobile Legends**: com.mobile.legends, com.vng.mlbbvn, com.mobilelegends.mi
    * **Call of Duty: Mobile**: com.proximabeta.mf.uamo, com.tencent.tmgp.cod, com.activision.callofduty.shooter, com.garena.game.codm, com.tencent.tmgp.kr.codm, com.vng.codmvn
    * **PUBG Mobile**: com.pubg.imobile (BGMI), com.tencent.ig (Global), com.pubg.krmobile, com.rekoo.pubgm, com.tencent.tmgp.pubgmhd, com.vng.pubgmobile
    * **Garena**: com.dts.freefiremax, com.dts.freefireth, com.garena.game.df (Delta Force)
    * **League of Legends: Wild Rift**: com.riotgames.league.wildrift, com.riotgames.league.wildrifttw, com.riotgames.league.wildriftvn, com.tencent.lolm
    * **Teamfight Tactics**: com.riotgames.league.teamfighttactics, com.riotgames.league.teamfighttacticstw, com.riotgames.league.teamfighttacticsvn
    * **Other games**:
        * Aether Gazer: com.YoStar.AetherGazer
        * Apex Legends Mobile: com.ea.gp.apexlegendsmobilefps
        * Asphalt: com.gameloft.android.ANMP.GloftA9HM (Asphalt 9), com.gameloft.android.ANMP.GloftA8HM (Asphalt 8)
        * Battle Prime: com.blitzteam.battleprime
        * Black Desert Mobile: com.pearlabyss.blackdesertm.gl, com.pearlabyss.blackdesertm
        * Brawl Stars: com.supercell.brawlstars
        * CarX Highway Racing: com.CarXTech.highWay
        * Clash of Clans: com.supercell.clashofclans
        * CrossFire: Legends: com.tencent.tmgp.cf
        * Dead Run 2: com.pikpok.dr2.play
        * Delta Force Mobile: com.proxima.dfm
        * Destiny Warfare: com.gamedevltd.destinywarfare
        * Diablo Immortal: com.blizzard.diablo.immortal
        * eFootball PES: jp.konami.pesam
        * FIFA Mobile: com.ea.gp.fifamobile
        * Fortnite: com.epicgames.fortnite, com.epicgames.portal
        * Honor of Kings: com.levelinfinite.sgameGlobal, com.tencent.tmgp.sgame
        * Identity V: com.netease.newspike
        * King of Fighters Allstar: com.tencent.KiHan
        * LifeAfter: com.netease.lztgglobal
        * Need for Speed: com.ea.game.nfs14_row
        * PUBG: New State: com.pubg.newstate
        * Real Racing 3: com.ea.games.r3_row
        * Shadow Fight: com.nekki.shadowfight3, com.nekki.shadowfightarena, com.nekki.shadowfight
        * Shadowgun Legends: com.madfingergames.legends
        * Squad Busters: com.supercell.squad
        * Tower of Fantasy: com.levelinfinite.hotta.gp
        * Warzone Mobile: com.activision.callofduty.warzone

--- app/src/main/java/orca/turbo/SafeKillerTileService.java
+++ app/src/main/java/orca/turbo/SafeKillerTileService.java
@@ -13,41 +13,41 @@ import java.io.DataOutputStream;
 @RequiresApi(api = Build.VERSION_CODES.N)
 public class SafeKillerTileService extends TileService {
 
-    @Override
-    public void onClick() {
-        unlockAndRun(() -> new Thread(() -> {
-            try {
-                Process p = Runtime.getRuntime().exec("su");
-                DataOutputStream os = new DataOutputStream(p.getOutputStream());
+    @Override
+    public void onClick() {
+        unlockAndRun(() -> new Thread(() -> {
+            try {
+                Process p = Runtime.getRuntime().exec("su");
+                DataOutputStream os = new DataOutputStream(p.getOutputStream());
 
-                // Grant POST_NOTIFICATIONS runtime permission
-                os.writeBytes("pm grant " + getPackageName() + " android.permission.POST_NOTIFICATIONS\n");
+                // Grant POST_NOTIFICATIONS runtime permission
+                os.writeBytes("pm grant " + getPackageName() + " android.permission.POST_NOTIFICATIONS\n");
 
-                // Force enable notifications using appops and settings
-                os.writeBytes("cmd appops set " + getPackageName() + " POST_NOTIFICATION allow\n");
-                os.writeBytes("settings put secure enabled_notification_list $(settings get secure enabled_notification_list)," + getPackageName() + "\n");
+                // Force enable notifications using appops and settings
+                os.writeBytes("cmd appops set " + getPackageName() + " POST_NOTIFICATION allow\n");
+                os.writeBytes("settings put secure enabled_notification_list $(settings get secure enabled_notification_list)," + getPackageName() + "\n");
 
-                // Kill user-installed apps except BGMI and MT
-                os.writeBytes("for pkg in $(pm list packages -3 | cut -d':' -f2); do\n");
-                os.writeBytes("  if [ \"$pkg\" != \"com.pubg.imobile\" ] && [ \"$pkg\" != \"com.tencent.ig\" ] && [ \"$pkg\" != \"com.pubg.krmobile\" ] && [ \"$pkg\" != \"com.vng.pubgmobile\" ] && [ \"$pkg\" != \"com.rekoo.pubgm\" ] && [ \"$pkg\" != \"com.pubg.newstate\" ] && [ \"$pkg\" != \"com.tencent.iglite\" ] && [ \"$pkg\" != \"com.dts.freefireth\" ] && [ \"$pkg\" != \"com.dts.freefiremax\" ] && [ \"$pkg\" != \"com.activision.callofduty.shooter\" ] && [ \"$pkg\" != \"com.epicgames.fortnite\" ] && [ \"$pkg\" != \"com.miHoYo.GenshinImpact\" ] && [ \"$pkg\" != \"com.HoYoverse.hkrpgoversea\" ] && [ \"$pkg\" != \"com.miHoYo.bh3global\" ] && [ \"$pkg\" != \"com.ea.gp.apexlegendsmobilefps\" ] && [ \"$pkg\" != \"com.kaixuan.blackholearena\" ] && [ \"$pkg\" != \"com.activision.warzonemobile\" ] && [ \"$pkg\" != \"com.blizzard.diablo.immortal\" ] && [ \"$pkg\" != \"com.netease.lifeafter\" ] && [ \"$pkg\" != \"com.pearlabyss.blackdesertm\" ] && [ \"$pkg\" != \"com.levelinfinite.hotta.gp\" ] && [ \"$pkg\" != \"com.kurogame.wutheringwaves\" ] && [ \"$pkg\" != \"com.nvsgames.DMC\" ]; then\n");
-                os.writeBytes("    am force-stop \"$pkg\"\n");
-                os.writeBytes("    killall -9 \"$pkg\" 2>/dev/null\n");
-                os.writeBytes("  fi\n");
-                os.writeBytes("done\n");
+                // Kill user-installed apps except for the specified games
+                os.writeBytes("for pkg in $(pm list packages -3 | cut -d':' -f2); do\n");
+                os.writeBytes("  if [ \"$pkg\" != \"com.proximabeta.mf.uamo\" ] && [ \"$pkg\" != \"com.mobile.legends\" ] && [ \"$pkg\" != \"com.tencent.KiHan\" ] && [ \"$pkg\" != \"com.tencent.tmgp.cf\" ] && [ \"$pkg\" != \"com.tencent.tmgp.cod\" ] && [ \"$pkg\" != \"com.tencent.tmgp.gnyx\" ] && [ \"$pkg\" != \"com.ea.gp.apexlegendsmobilefps\" ] && [ \"$pkg\" != \"com.levelinfinite.hotta.gp\" ] && [ \"$pkg\" != \"com.supercell.clashofclans\" ] && [ \"$pkg\" != \"com.vng.mlbbvn\" ] && [ \"$pkg\" != \"com.levelinfinite.sgameGlobal\" ] && [ \"$pkg\" != \"com.tencent.tmgp.sgame\" ] && [ \"$pkg\" != \"com.pubg.krmobile\" ] && [ \"$pkg\" != \"com.rekoo.pubgm\" ] && [ \"$pkg\" != \"com.tencent.tmgp.pubgmhd\" ] && [ \"$pkg\" != \"com.vng.pubgmobile\" ] && [ \"$pkg\" != \"com.pubg.imobile\" ] && [ \"$pkg\" != \"com.tencent.ig\" ] && [ \"$pkg\" != \"com.YoStar.AetherGazer\" ] && [ \"$pkg\" != \"com.netease.lztgglobal\" ] && [ \"$pkg\" != \"com.riotgames.league.wildrift\" ] && [ \"$pkg\" != \"com.riotgames.league.wildrifttw\" ] && [ \"$pkg\" != \"com.riotgames.league.wildriftvn\" ] && [ \"$pkg\" != \"com.epicgames.fortnite\" ] && [ \"$pkg\" != \"com.epicgames.portal\" ] && [ \"$pkg\" != \"com.tencent.lolm\" ] && [ \"$pkg\" != \"jp.konami.pesam\" ] && [ \"$pkg\" != \"com.dts.freefiremax\" ] && [ \"$pkg\" != \"com.dts.freefireth\" ] && [ \"$pkg\" != \"com.ea.gp.fifamobile\" ] && [ \"$pkg\" != \"com.pearlabyss.blackdesertm.gl\" ] && [ \"$pkg\" != \"com.pearlabyss.blackdesertm\" ] && [ \"$pkg\" != \"com.activision.callofduty.shooter\" ] && [ \"$pkg\" != \"com.gameloft.android.ANMP.GloftA9HM\" ] && [ \"$pkg\" != \"com.madfingergames.legends\" ] && [ \"$pkg\" != \"com.riotgames.league.teamfighttactics\" ] && [ \"$pkg\" != \"com.riotgames.league.teamfighttacticstw\" ] && [ \"$pkg\" != \"com.riotgames.league.teamfighttacticsvn\" ] && [ \"$pkg\" != \"com.garena.game.codm\" ] && [ \"$pkg\" != \"com.tencent.tmgp.kr.codm\" ] && [ \"$pkg\" != \"com.vng.codmvn\" ] && [ \"$pkg\" != \"com.mobilelegends.mi\" ] && [ \"$pkg\" != \"com.supercell.brawlstars\" ] && [ \"$pkg\" != \"com.blizzard.diablo.immortal\" ] && [ \"$pkg\" != \"com.netease.newspike\" ] && [ \"$pkg\" != \"com.activision.callofduty.warzone\" ] && [ \"$pkg\" != \"com.pubg.newstate\" ] && [ \"$pkg\" != \"com.gamedevltd.destinywarfare\" ] && [ \"$pkg\" != \"com.pikpok.dr2.play\" ] && [ \"$pkg\" != \"com.CarXTech.highWay\" ] && [ \"$pkg\" != \"com.nekki.shadowfight3\" ] && [ \"$pkg\" != \"com.nekki.shadowfightarena\" ] && [ \"$pkg\" != \"com.gameloft.android.ANMP.GloftA8HM\" ] && [ \"$pkg\" != \"com.nekki.shadowfight\" ] && [ \"$pkg\" != \"com.ea.game.nfs14_row\" ] && [ \"$pkg\" != \"com.ea.games.r3_row\" ] && [ \"$pkg\" != \"com.supercell.squad\" ] && [ \"$pkg\" != \"com.blitzteam.battleprime\" ] && [ \"$pkg\" != \"com.garena.game.df\" ] && [ \"$pkg\" != \"com.proxima.dfm\" ]; then\n");
+                os.writeBytes("    am force-stop \"$pkg\"\n");
+                os.writeBytes("    killall -9 \"$pkg\" 2>/dev/null\n");
+                os.writeBytes("  fi\n");
+                os.writeBytes("done\n");
 
-                // Boost BGMI
-                os.writeBytes("am set-foreground-service com.pubg.imobile 1\n");
-                os.writeBytes("dumpsys deviceidle whitelist +com.pubg.imobile\n");
+                // Boost BGMI
+                os.writeBytes("am set-foreground-service com.pubg.imobile 1\n");
+                os.writeBytes("dumpsys deviceidle whitelist +com.pubg.imobile\n");
 
-                os.writeBytes("exit\n");
-                os.flush();
-                os.close();
+                os.writeBytes("exit\n");
+                os.flush();
+                os.close();
 
-                new Handler(Looper.getMainLooper()).post(() -> Toast.makeText(SafeKillerTileService.this, "Turbo Activated", Toast.LENGTH_SHORT).show());
+                new Handler(Looper.getMainLooper()).post(() -> Toast.makeText(SafeKillerTileService.this, "Turbo Activated", Toast.LENGTH_SHORT).show());
 
-            } catch (Exception e) {
-                new Handler(Looper.getMainLooper()).post(() -> Toast.makeText(SafeKillerTileService.this, "Root required!", Toast.LENGTH_SHORT).show());
-            }
-        }).start());
-    }
+            } catch (Exception e) {
+                new Handler(Looper.getMainLooper()).post(() -> Toast.makeText(SafeKillerTileService.this, "Root required!", Toast.LENGTH_SHORT).show());
+            }
+        }).start());
+    }
 }
